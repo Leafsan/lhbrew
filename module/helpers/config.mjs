1b0e0adb97b01edf1670ed255c86496a855849ed
Leafsan <cwk0906@naver.com>
2024-08-24 16:02:45 +0900

    first commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,57 @@
+export const LHTRPG = {};
+
+LHTRPG.races = {
+  none: "LHTRPG.Races.none",
+  human: "LHTRPG.Races.human",
+  elf: "LHTRPG.Races.elf",
+  dwarf: "LHTRPG.Races.dwarf",
+  halfAlv: "LHTRPG.Races.halfAlv",
+  felinoid: "LHTRPG.Races.felinoid",
+  wolfFang: "LHTRPG.Races.wolfFang",
+  foxTail: "LHTRPG.Races.foxTail",
+};
+
+LHTRPG.classes = {
+  none: "LHTRPG.Classes.none",
+  guardian: "LHTRPG.Classes.guardian",
+  samurai: "LHTRPG.Classes.samurai",
+  monk: "LHTRPG.Classes.monk",
+  hwarang: "LHTRPG.Classes.hwarang",
+  cleric: "LHTRPG.Classes.cleric",
+  druid: "LHTRPG.Classes.druid",
+  kannagi: "LHTRPG.Classes.kannagi",
+  templar: "LHTRPG.Classes.templar",
+  assassin: "LHTRPG.Classes.assassin",
+  swashbuckler: "LHTRPG.Classes.swashbuckler",
+  bard: "LHTRPG.Classes.bard",
+  sorcerer: "LHTRPG.Classes.sorcerer",
+  summoner: "LHTRPG.Classes.summoner",
+  enchanter: "LHTRPG.Classes.enchanter",
+};
+
+LHTRPG.ASCII = `
+              _-o#&&*''''?d:>b\_
+          _o/"'''  '',, dMF9MMMMMHo_
+       .o&#'        '"MbHMMMMMMMMMMMHo.
+     .o"" '         vodM*$&&HMMMMMMMMMM?.
+    ,'              $M&ood,~''(&##MMMMMMH\\
+   /               ,MMMMMMM#b?#bobMMMMHMMML
+  &              ?MMMMMMMMMMMMMMMMM7MMM$R*Hk
+ ?$.            :MMMMMMMMMMMMMMMMMMM/HMMM|'*L
+|               |MMMMMMMMMMMMMMMMMMMMbMH'   T,
+$H#:            '*MMMMMMMMMMMMMMMMMMMMb#}'  '?
+]MMH#             ""*""""*#MMMMMMMMMMMMM'    -
+MMMMMb_                   |MMMMMMMMMMMP'     :
+HMMMMMMMHo                 'MMMMMMMMMT       .
+?MMMMMMMMP                  9MMMMMMMM}       -
+-?MMMMMMM                  |MMMMMMMMM?,d-    '
+ :|MMMMMM-                 'MMMMMMMT .M|.   :
+  .9MMM[                    &MMMMM*' ''    .
+   :9MMk                    'MMM#"        -
+     &M}                     '          .-
+      '&.                             .
+        '~,   .                     ./
+            . _                  .-
+              ''--._,dd###pp=""'
+
+`;
