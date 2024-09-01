a7de9fb9b23b682acbe60d1f615e3554fcd436f8
Leafsan <cwk0906@naver.com>
2024-09-01 20:01:48 +0900

    added json load util

---
+++ module/utils/load-json.mjs
@@ -0,0 +1,13 @@
+export async function loadJSONData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching JSON data:", error);
+  }
+}
